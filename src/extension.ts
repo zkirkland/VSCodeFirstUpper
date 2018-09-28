150d304e7c128d83d4db6027e578c67a49c48e37
Zach Kirkland <zkirkland@users.noreply.github.com>
2018-09-28 11:55:53 -0500

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,27 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { commands, window, ExtensionContext, Position, Range, Selection, TextDocument, workspace, Uri } from 'vscode';
-const path = require('path');
+import * as vscode from 'vscode';
+import * as path from 'path';
 
-const prefix = 'markdown.extension.editing.';
-var ignoreCharacters = [' ', '\t', '\n', '.', '!', '?', '#', '\r', ','];
-var forbiddenWords = [''];
+var forbiddenWords:string[] = [];
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
-    commands.registerCommand('extension.capitalizeTitle', capitalizeTitle);
-    commands.registerCommand('extension.capitalizeMarkDownTitles', capitalizeMDTitles);
+export function activate(context: vscode.ExtensionContext) {
+    vscode.commands.registerCommand('extension.capitalizeTitle', capitalizeTitle);
+    vscode.commands.registerCommand('extension.capitalizeMarkDownTitles', capitalizeMDTitles);
 }
 
 function getForbiddenWords() {
     forbiddenWords.pop();
-    let fileUri = Uri.file(path.join(__dirname, '../../data/FirstUpperForbiddenWords.txt'));
-    return workspace.openTextDocument(fileUri).then((document) => {
+    let fileUri = vscode.Uri.file(path.join(__dirname, '../data/FirstUpperForbiddenWords.txt'));
+    return vscode.workspace.openTextDocument(fileUri).then((document) => {
         for (var index = 0; index < document.lineCount; index++) {
 
             let text = document.lineAt(index).text;
 
-            if (text.toLowerCase().charAt(0) != '*') {
+            if (text.toLowerCase().charAt(0) !== '*') {
                 forbiddenWords.push(text);
             }
         }
@@ -30,16 +29,16 @@ function getForbiddenWords() {
 }
 
 function capitalizeTitle() {
-    if (forbiddenWords.length == 1) {
-        getForbiddenWords().then(capitalizeFirstLetters);
+    if (forbiddenWords.length === 0) {
+        getForbiddenWords().then(capitalizeFirstLettersOfSelection);
     }
     else {
-        capitalizeFirstLetters();
+        capitalizeFirstLettersOfSelection();
     }
 }
 
 function capitalizeMDTitles() {
-    if (forbiddenWords.length == 1) {
+    if (forbiddenWords.length === 0) {
         getForbiddenWords().then(capitalizeMDFirstLetters);
     }
     else {
@@ -47,67 +46,85 @@ function capitalizeMDTitles() {
     }
 }
 
-function capitalizeFirstLetters(line = null): string {
-    let editor = window.activeTextEditor;
+function capitalizeFirstLetters(line:string): string {
+    let editor = vscode.window.activeTextEditor;
+
+    if (!editor) { return ""; }
+
+    let selectedTextArrayOfWords = line.split(" ");
+    let firstWord = true;
+    let newLineOfText = "";
+    for (let index = 0; index < selectedTextArrayOfWords.length; index++) {
+        const word = selectedTextArrayOfWords[index];
+
+        let upperWord:string = "";
+        if ((firstWord || forbiddenWords.indexOf(word) === -1) && isOnlyAlpha(word)) {
+            upperWord = word[0].toLocaleUpperCase() + word.substr(1);
+            firstWord = false;
+            newLineOfText += index < selectedTextArrayOfWords.length - 1 ? upperWord + " " : upperWord;
+        } else {
+            newLineOfText += index < selectedTextArrayOfWords.length - 1 ? word + " " : word;
+        }
+    }
+    
+    return newLineOfText;
+}
+
+function capitalizeFirstLettersOfSelection(): string {
+    let editor = vscode.window.activeTextEditor;
+
+    if(!editor) { return ""; }
+
     let selection = editor.selection;
     let selectedText = editor.document.getText(selection);
     selectedText = selectedText.toLocaleLowerCase();
 
-    let newLineOfText = "";
+    let selectedTextArrayOfWords = selectedText.split(" ");
     let firstWord = true;
-    let selectedTextArrayOfWords;
-    
-    if (line == null) {
-        selectedTextArrayOfWords = selectedText.split(" ");
-    }
-    else
-    {
-        selectedTextArrayOfWords = line.split(" ");
-    }
-    
-    selectedTextArrayOfWords.forEach((word) => {
-        if ((firstWord || forbiddenWords.indexOf(word) == -1) && isOnlyAlpha(word)) {
-            word = word[0].toLocaleUpperCase() + word.substr(1);
+    let newLineOfText = "";
+    for (let index = 0; index < selectedTextArrayOfWords.length; index++) {
+        const word = selectedTextArrayOfWords[index];
+
+        if ((firstWord || forbiddenWords.indexOf(word) === -1) && isOnlyAlpha(word)) {
+            let upperWord = word[0].toLocaleUpperCase() + word.substr(1);
             firstWord = false;
+
+            newLineOfText += index < selectedTextArrayOfWords.length - 1 ? upperWord + " " : upperWord;
+        } else {
+            newLineOfText += index < selectedTextArrayOfWords.length - 1 ? word + " " : word;
         }
-        
-        newLineOfText += word + " ";
-    });
+    }
     
-    if (line == null) {
-        replaceSelectionWith(selection, newLineOfText.trim());
-        return newLineOfText.trim();
-    }
-    else {
-        return newLineOfText.trim();
-    }
+    replaceSelectionWith(selection, newLineOfText);
+    return newLineOfText;
 }
 
 function capitalizeMDFirstLetters() {
-    let editor = window.activeTextEditor;
+    let editor = vscode.window.activeTextEditor;
+
+    if (!editor) { return; }
+
     let line = '';
     let numberOfLines = editor.document.lineCount;
     
     let lastLineLastCharacter = editor.document.lineAt(numberOfLines - 1).text.length;
     
-    editor.selection = new Selection(new Position(0,0), new Position(numberOfLines - 1, lastLineLastCharacter));
+    editor.selection = new vscode.Selection(new vscode.Position(0,0), new vscode.Position(numberOfLines - 1, lastLineLastCharacter));
     
-    let listOfLines = editor.document.getText(editor.selection).split("\n");
+    let selectionText = editor.document.getText(editor.selection);
+    let listOfLines = selectionText.split("\r\n");
     
     for (var i = 0; i < listOfLines.length; i++) {
         line = listOfLines[i];
-        if (line[0] == "#") {
-            listOfLines[i] = capitalizeFirstLetters(line) + "\n";
+        if (line[0] === "#") {
+            listOfLines[i] = capitalizeFirstLetters(line);
         }
-        else if ((line[0] == '-' || line[0] == '=') && !isAlphaNumeric(line)) {
-            listOfLines[i - 1] = capitalizeFirstLetters(listOfLines[i - 1]) + "\n";
+        else if ((line[0] === '-' || line[0] === '=') && !isAlphaNumeric(line)) {
+            listOfLines[i - 1] = capitalizeFirstLetters(listOfLines[i - 1]);
         }
     }
     
-    let newDocumentText = '';
-    listOfLines.forEach((line) => {
-        newDocumentText += line;
-    });
+    let newDocumentText = listOfLines.join("\r\n");
     
     replaceSelectionWith(editor.selection, newDocumentText);
 }
@@ -122,8 +139,11 @@ function isOnlyAlpha(line: string): boolean {
     return rg.test(line);
 }
 
-function replaceSelectionWith(selection: Selection, newText: string) {
-    let editor = window.activeTextEditor;
+function replaceSelectionWith(selection: vscode.Selection, newText: string) {
+    let editor = vscode.window.activeTextEditor;
+    
+    if (!editor) { return; }
+
     editor.edit((edit) => {
         edit.replace(selection, newText);
     });
